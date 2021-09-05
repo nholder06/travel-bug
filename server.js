f551bcfde049cd93a198e080da920cc829b06ab6
Nicole Holder <nholder06@gmail.com>
2021-09-04 19:57:30 -0500

    First commit. WIP creating server using koa-NH

---
+++ server.js
@@ -0,0 +1,24 @@
+const Koa = require("koa");
+const BodyParser = require("koa-bodyparser");
+const Router = require("koa-router");
+const Logger = require("koa-logger");
+const cors = require("koa-cors");
+const serve = require("koa-static");
+const mount = require("koa-mount");
+const HttpStatus = require("http-status");
+
+const app = new Koa();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(BodyParser());
+app.use(Logger());
+app.use(cors());
+
+app.listen(PORT, function() {
+  console.log(
+    "==> 🌎  Listening on port %s. Visit http://localhost:%s/",
+    PORT,
+    PORT
+  );
+});
